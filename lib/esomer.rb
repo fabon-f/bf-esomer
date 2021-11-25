d5a90f55159d594b27f4a01de09506d72ae1835d
fabon <syobon.hinata.public@gmail.com>
2021-11-26 05:30:25 +0900

    Add code generator

--- lib/esomer.rb
+++ lib/esomer.rb
@@ -3,6 +3,7 @@
 require_relative "esomer/version"
 require_relative "esomer/ast"
 require_relative "esomer/parser"
+require_relative "esomer/codegen"
 
 module Esomer
   # Your code goes here...
---
+++ lib/esomer/codegen.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+module Esomer
+  class CodeGenerator
+    def cpp_header
+      <<-CPP
+#include <cstdio>
+#include <cstdint>
+#include <array>
+
+std::array<std::uint8_t,100001> tape;
+std::int32_t pos;
+
+inline uint8_t readchar() {
+  std::fflush(stdout);
+  int c = std::fgetc(stdin);
+  return c == EOF ? 255 : c;
+}
+
+inline void printchar(uint8_t c) {
+  std::fputc(c, stdout);
+}
+
+void exec();
+
+int main() {
+  tape.fill(0);
+  pos = 50000;
+
+  exec();
+}
+
+      CPP
+    end
+
+    def generate_cpp_proc(ast, indent_level = 1)
+      indent = "  " * indent_level
+      codes = ast.children.map do |node|
+        case node
+        when AST::MoveRelative
+          "#{indent}pos += #{node.offset};"
+        when AST::AddHere
+          "#{indent}tape[pos] += #{node.value};"
+        when AST::PrintHere
+          "#{indent}printchar(tape[pos]);"
+        when AST::ReadHere
+          "#{indent}tape[pos] = readchar();"
+        when AST::WhileLoop
+          "#{indent}while(tape[pos]) {\n#{generate_cpp_proc(node, indent_level + 1)}\n#{indent}}";
+        else
+          raise "Invalid AST"
+        end
+      end
+
+      codes.join("\n")
+    end
+
+    def generate_cpp(ast)
+      "#{cpp_header}void exec() {\n#{generate_cpp_proc(ast)}\n}\n"
+    end
+  end
+end
