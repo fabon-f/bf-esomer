8c18053ccebb7b2cf3623adcf8793db8a8a2bba7
fabon <syobon.hinata.public@gmail.com>
2021-11-25 23:47:04 +0900

    Improve type annotation

--- test/parser_test.rb
+++ test/parser_test.rb
@@ -7,20 +7,40 @@ class EsomerTest < Test::Unit::TestCase
     parser = ::Esomer::Parser.new
     ast = parser.parse(">++++[<+>-]")
     assert_instance_of(::Esomer::AST::Program, ast)
-    assert_instance_of(::Esomer::AST::MoveRelative, ast.children[0])
-    assert_equal(1, ast.children[0].offset)
-    assert_instance_of(::Esomer::AST::AddHere, ast.children[1])
-    assert_equal(4, ast.children[1].value)
+
+    node1 = ast.children[0]
+    assert_instance_of(::Esomer::AST::MoveRelative, node1)
+    raise unless node1.instance_of?(::Esomer::AST::MoveRelative)
+    assert_equal(1, node1.offset)
+
+    node2 = ast.children[1]
+    assert_instance_of(::Esomer::AST::AddHere, node2)
+    raise unless node2.instance_of?(::Esomer::AST::AddHere)
+    assert_equal(4, node2.value)
+
     assert_instance_of(::Esomer::AST::WhileLoop, ast.children[2])
     loop_ast = ast.children[2]
-    assert_instance_of(::Esomer::AST::MoveRelative, loop_ast.children[0])
-    assert_equal(-1, loop_ast.children[0].offset)
-    assert_instance_of(::Esomer::AST::AddHere, loop_ast.children[1])
-    assert_equal(1, loop_ast.children[1].value)
-    assert_instance_of(::Esomer::AST::MoveRelative, loop_ast.children[2])
-    assert_equal(1, loop_ast.children[2].offset)
-    assert_instance_of(::Esomer::AST::AddHere, loop_ast.children[3])
-    assert_equal(-1, loop_ast.children[3].value)
+    raise unless loop_ast.instance_of?(::Esomer::AST::WhileLoop)
+
+    node3 = loop_ast.children[0]
+    assert_instance_of(::Esomer::AST::MoveRelative, node3)
+    raise unless node3.instance_of?(::Esomer::AST::MoveRelative)
+    assert_equal(-1, node3.offset)
+
+    node4 = loop_ast.children[1]
+    assert_instance_of(::Esomer::AST::AddHere, node4)
+    raise unless node4.instance_of?(::Esomer::AST::AddHere)
+    assert_equal(1, node4.value)
+
+    node5 = loop_ast.children[2]
+    assert_instance_of(::Esomer::AST::MoveRelative, node5)
+    raise unless node5.instance_of?(::Esomer::AST::MoveRelative)
+    assert_equal(1, node5.offset)
+
+    node6 = loop_ast.children[3]
+    assert_instance_of(::Esomer::AST::AddHere, node6)
+    raise unless node6.instance_of?(::Esomer::AST::AddHere)
+    assert_equal(-1, node6.value)
   end
 
   test "parser should fail with unbalanced brackets" do
