0984868dd35c9be6c589b3f7ffd445625e759477
fabon <syobon.hinata.public@gmail.com>
2021-11-26 18:59:03 +0900

    Add optimization level option

--- lib/esomer/compiler.rb
+++ lib/esomer/compiler.rb
@@ -4,12 +4,13 @@ require "open3"
 
 module Esomer
   class Compiler
-    def build_cpp(code, dest, compiler: "gcc")
+    def build_cpp(code, dest, compiler: "gcc", optimize_level: 0)
+      raise "Invalid optimization level: #{optimize_level}" if [0,1,2].include?(optimize_level)
       cmd = case compiler
       when "gcc"
-        ["g++","-o", dest, "-x", "c++", "-O2", "-std=c++11", "-"]
+        ["g++","-o", dest, "-x", "c++", "-O#{optimize_level}", "-std=c++11", "-"]
       when "clang"
-        ["clang++", "-o", dest, "-x", "c++", "-O2", "-std=c++11", "-"]
+        ["clang++", "-o", dest, "-x", "c++", "-O#{optimize_level}", "-std=c++11", "-"]
       else
         raise "Unsupported C compiler"
       end
@@ -21,10 +22,10 @@ module Esomer
       end
     end
 
-    def build_executable(bf_code, dest)
+    def build_executable(bf_code, dest, optimize_level: 0)
       ast = Parser.new.parse(bf_code)
       cpp_code = CodeGenerator.new.generate_cpp(ast)
-      build_cpp(cpp_code, dest)
+      build_cpp(cpp_code, dest, optimize_level: optimize_level)
     end
   end
 end
